4f6dde035ef0a1932ee6d119e5e1ff8735281480
Your Name <you@example.com>
2025-10-05 10:58:27 +0200

    Created a image gallery

--- image-gallery/src/App.tsx
+++ image-gallery/src/App.tsx
@@ -1,34 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  type Image = {
+    id: string;
+    author: string;
+    download_url: string;
+    // add other fields if needed
+  };
+
+  const [images, setImages] = useState<Image[]>([]); 
+  const [page, setPage] = useState(1);
+
+  const fetchImages = async () => {
+      try {
+        const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=10`);
+        console.log(response.data);
+        console.log('page:', page);
+        setImages((prevImages) => [...prevImages, ...response.data]);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  const style = {
+    borderRadius: '20px',
+    width: '200px',
+    marginBottom: '20px',
+    // border: '5px solid white',
+    borderLeft: '5px solid white',
+    borderBottom: '5px solid white',
+   
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ display: 'flex', gap: '20px' }}> 
+    <InfiniteScroll
+    dataLength={images.length} //This is important field to render the next data
+    next={() => setPage((prev) => prev + 1)}
+    hasMore={true}
+    loader={<h4>Loading...</h4>}>
+      {images.map((image: any) => (
+        <div key={image.id} className="image-card">
+          <img src={image.download_url} alt={image.author} style={style} loading='lazy' />
+        </div>
+      ))}
+      </InfiniteScroll>
+ 
+        <InfiniteScroll
+          dataLength={images.length} //This is important field to render the next data
+          next={() => setPage((prev) => prev + 1)}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}>
+            {images.map((image: any) => (
+              <div key={image.id} className="image-card">
+                <img src={image.download_url} alt={image.author} style={style} loading='lazy' />
+              </div>
+            ))}
+        </InfiniteScroll>
+     
+    
+        <InfiniteScroll
+          dataLength={images.length} //This is important field to render the next data
+          next={() => setPage((prev) => prev + 1)}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}>
+            {images.map((image: any) => (
+              <div key={image.id} className="image-card">
+                <img src={image.download_url} alt={image.author} style={style} loading='lazy' />
+              </div>
+            ))}
+        </InfiniteScroll>
+     
+    </div>
   )
 }
 
