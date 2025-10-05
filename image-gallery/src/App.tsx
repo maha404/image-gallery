fdb2e632873be3fb8d8efe27d476e6adf66e69b4
maha404 <maria_halvarsson@hotmail.se>
2025-10-05 17:27:50 +0200

    updated the code with better performance

--- image-gallery/src/App.tsx
+++ image-gallery/src/App.tsx
@@ -1,25 +1,22 @@
 import './App.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { Suspense, lazy } from 'react';
+
+const Images = lazy(() => import('./Image'));
 
 function App() {
-
-  type Image = {
-    id: string;
-    author: string;
-    download_url: string;
-    // add other fields if needed
-  };
 
   const [images, setImages] = useState<Image[]>([]); 
   const [page, setPage] = useState(1);
 
+  type Image = {
+    id: string;
+  };
+
   const fetchImages = async () => {
       try {
         const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=10`);
-        console.log(response.data);
-        console.log('page:', page);
         setImages((prevImages) => [...prevImages, ...response.data]);
       } catch (error) {
         console.error('Error fetching images:', error);
@@ -29,56 +26,35 @@ function App() {
   useEffect(() => {
     fetchImages();
   }, [page]);
-
-  const style = {
-    borderRadius: '20px',
-    width: '200px',
-    marginBottom: '20px',
-    // border: '5px solid white',
-    borderLeft: '5px solid white',
-    borderBottom: '5px solid white',
-   
-  };
   
   return (
-    <div style={{ display: 'flex', gap: '20px' }}> 
-    <InfiniteScroll
-    dataLength={images.length} //This is important field to render the next data
-    next={() => setPage((prev) => prev + 1)}
-    hasMore={true}
-    loader={<h4>Loading...</h4>}>
-      {images.map((image: any) => (
-        <div key={image.id} className="image-card">
-          <img src={image.download_url} alt={image.author} style={style} loading='lazy' />
-        </div>
-      ))}
-      </InfiniteScroll>
- 
-        <InfiniteScroll
-          dataLength={images.length} //This is important field to render the next data
-          next={() => setPage((prev) => prev + 1)}
+    <div className="image-gallery"> 
+      <Suspense fallback={<div>Loading...</div>}>
+        <Images 
+          id="scrollableDiv"
+          dataLength={images.length}
+          next={() => setPage((prevPage) => prevPage + 1)}
           hasMore={true}
-          loader={<h4>Loading...</h4>}>
-            {images.map((image: any) => (
-              <div key={image.id} className="image-card">
-                <img src={image.download_url} alt={image.author} style={style} loading='lazy' />
-              </div>
-            ))}
-        </InfiniteScroll>
-     
-    
-        <InfiniteScroll
-          dataLength={images.length} //This is important field to render the next data
-          next={() => setPage((prev) => prev + 1)}
+          loader={<h4>Loading...</h4>}
+          dataArray={images}
+        />
+        <Images 
+          id="scrollableDiv"
+          dataLength={images.length}
+          next={() => setPage((prevPage) => prevPage + 1)}
           hasMore={true}
-          loader={<h4>Loading...</h4>}>
-            {images.map((image: any) => (
-              <div key={image.id} className="image-card">
-                <img src={image.download_url} alt={image.author} style={style} loading='lazy' />
-              </div>
-            ))}
-        </InfiniteScroll>
-     
+          loader={<h4>Loading...</h4>}
+          dataArray={images}
+        />
+        <Images 
+          id="scrollableDiv"
+          dataLength={images.length}
+          next={() => setPage((prevPage) => prevPage + 1)}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          dataArray={images}
+        />
+      </Suspense>
     </div>
   )
 }
---
+++ image-gallery/src/Image.tsx
@@ -0,0 +1,26 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+interface ImageProps {
+  id: string;
+  dataLength: number;
+  next: () => void;
+  hasMore: boolean;
+  loader: React.ReactNode;
+  dataArray?: any[];
+}
+
+export default ({ id, dataLength, next, hasMore, loader, dataArray }: ImageProps) => {
+return (
+     <InfiniteScroll
+        dataLength={dataLength}
+        next={next}
+        hasMore={hasMore}
+        loader={loader}>
+          {dataArray?.map((image: any) => (
+            <div key={image.id}>
+              <img className="image" src={image.download_url} alt={image.author} />
+            </div>
+          ))}
+          </InfiniteScroll>
+  );
+}
